641dc35874b9e99435dc3270853136b743e210ac
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-06-27 19:12:41 +0300

    ADD: Add the i18n library

--- src/shared/config/routes/routesConfig.tsx
+++ src/shared/config/routes/routesConfig.tsx
@@ -4,28 +4,28 @@ import { HomePage } from 'pages/Home'
 import { RouteProps } from 'react-router-dom'
 
 export enum Routes {
-	HOME = 'home',
-	ABOUT = 'about',
-	CONTACTS = 'contacts',
+  HOME = 'home',
+  ABOUT = 'about',
+  CONTACTS = 'contacts',
 }
 
 export const RoutesPath: Record<Routes, string> = {
-	[Routes.HOME]: '/',
-	[Routes.ABOUT]: '/about',
-	[Routes.CONTACTS]: '/contacts',
+  [Routes.HOME]: '/',
+  [Routes.ABOUT]: '/about',
+  [Routes.CONTACTS]: '/contacts',
 }
 
 export const routesConfig: Record<Routes, RouteProps> = {
-	[Routes.HOME]: {
-		path: RoutesPath[Routes.HOME],
-		element: <HomePage />,
-	},
-	[Routes.ABOUT]: {
-		path: RoutesPath[Routes.ABOUT],
-		element: <AboutPage />,
-	},
-	[Routes.CONTACTS]: {
-		path: RoutesPath[Routes.CONTACTS],
-		element: <ContactsPage />,
-	},
+  [Routes.HOME]: {
+    path: RoutesPath[Routes.HOME],
+    element: <HomePage />,
+  },
+  [Routes.ABOUT]: {
+    path: RoutesPath[Routes.ABOUT],
+    element: <AboutPage />,
+  },
+  [Routes.CONTACTS]: {
+    path: RoutesPath[Routes.CONTACTS],
+    element: <ContactsPage />,
+  },
 }
