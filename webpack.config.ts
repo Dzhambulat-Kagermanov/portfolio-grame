ec14416a57cab344e4c3a795244e4c78a72ffbf2
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-06-27 19:11:06 +0300

    UPDATE: Update the package.json and webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,23 @@
 import webpack from 'webpack'
-import { buildWebpack } from './config/build/buildConfig'
 import path from 'path'
+import { buildWebpack } from './config/build/buildConfig'
 import { IBuildEnv } from './config/build/types/config'
 
 const config = (env: IBuildEnv): webpack.Configuration => {
-	const isProd: boolean = env.mode === 'production'
-	const isDev: boolean = !isProd
+  const isProd: boolean = env.mode === 'production'
+  const isDev: boolean = !isProd
 
-	return buildWebpack({
-		mode: env.mode,
-		paths: {
-			entry: path.resolve(__dirname, 'src', 'index.tsx'),
-			output: path.resolve(__dirname, 'build'),
-			html: path.resolve(__dirname, 'public', 'index.html'),
-			src: path.resolve(__dirname, 'src'),
-		},
-		isDev,
-		isProd,
-		port: env.port,
-	})
+  return buildWebpack({
+    mode: env.mode,
+    paths: {
+      entry: path.resolve(__dirname, 'src', 'index.tsx'),
+      output: path.resolve(__dirname, 'build'),
+      html: path.resolve(__dirname, 'public', 'index.html'),
+      src: path.resolve(__dirname, 'src'),
+    },
+    isDev,
+    isProd,
+    port: env.port,
+  })
 }
 export default config
